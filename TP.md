f805cb5f997f7091a9d11fc897fd4b9ba695cdd8
BlackB34rd <contact@the-tiny-spark.com>
2026-06-02 10:19:24 +0200

    add tp n°1

---
+++ TP.md
@@ -0,0 +1,11 @@
+TP n°1 
+
+Remplir le composant IdCardComponent avec les informations 
+suivantes: 
+
+
+- Nom
+- Prenom
+- Age 
+- Sexe
+- Profession
--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet, RouterLink } from '@angular/router';
+import { IdCardComponent } from '../id-card/id-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink],
+  imports: [CommonModule, RouterOutlet, RouterLink, IdCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'MyApp';
+  public title:string = 'My Video Game Store';
 }
