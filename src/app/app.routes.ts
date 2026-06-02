8d54e57b5f14587b1b476ff65c04e22f32b41bdc
BlackB34rd <contact@the-tiny-spark.com>
2026-06-02 11:30:03 +0200

    add tp n°2 correction

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/screens/home/home.component';
+import { CatalogComponent } from './components/screens/catalog/catalog.component';
+import { CartComponent } from './components/screens/cart/cart.component';
+import { LoginComponent } from './components/screens/login/login.component';
 
+// Configuration of the routes of the application
 const routeConfig: Routes = [
   {
     path: '',
@@ -11,6 +15,21 @@ const routeConfig: Routes = [
     path: 'home',
     component: HomeComponent,
     title: 'Home page'
+  },
+  {
+    path: 'catalog',
+    component: CatalogComponent,
+    title: 'Catalog page'
+  },
+  {
+    path: 'cart',
+    component: CartComponent,
+    title: 'Cart page'
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    title: 'Login page'
   }
 ];
 
