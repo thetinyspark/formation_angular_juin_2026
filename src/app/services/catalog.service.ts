21dfa78c5c2baada17fea9c39afcd910e32cae0c
BlackB34rd <contact@the-tiny-spark.com>
2026-06-04 14:09:37 +0200

    update tp n°16

--- src/app/services/catalog.service.ts
+++ src/app/services/catalog.service.ts
@@ -1,7 +1,6 @@
 import { inject, Injectable } from '@angular/core';
 import { Product } from '../model/product';
-import { PRODUCTS } from '../model/mocks/product.mock';
-import { firstValueFrom, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 
@@ -13,17 +12,9 @@ export class CatalogService {
   private _httpClient: HttpClient = inject(HttpClient);
 
 
-  public getProducts():Product[] {
-    return PRODUCTS;
-  }
 
   public getProductsFromAPI():Observable<Product[]> {
     return this._httpClient.get<Product[]>(environment.catalogURL);
-  }
-
-  // transforme un observable en promesse
-  public getProductsFromAPIPromise():Promise<Product[]>{
-    return firstValueFrom(this.getProductsFromAPI());
   }
 
 }
