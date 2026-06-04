25f1bcc7bdcd04adfe1c1353192b9e2db0eef7bf
BlackB34rd <contact@the-tiny-spark.com>
2026-06-04 14:43:59 +0200

    add tp n°16 correction

--- src/app/services/catalog.service.spec.ts
+++ src/app/services/catalog.service.spec.ts
@@ -1,16 +1,42 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CatalogService } from './catalog.service';
+import { Observable, of } from 'rxjs';
+import { PRODUCTS } from '../model/mocks/product.mock';
+import { Product } from '../model/product';
+import { HttpClient } from '@angular/common/http';
 
-describe('CatalogService', () => {
+fdescribe('CatalogService', () => {
   let service: CatalogService;
 
+  class FakeHttpClient{
+    public get(url:string):Observable<Product[]>{
+      return of(PRODUCTS);
+    }
+  }; 
+
+  const fakeHttpClient = new FakeHttpClient();
+
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        {provide: HttpClient, useValue: fakeHttpClient}
+      ]
+    });
     service = TestBed.inject(CatalogService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should get products from http client', () => {
+    
+    service.getProductsFromAPI().subscribe( 
+      (products:Product[])=>{
+        expect(products).toEqual(PRODUCTS);
+      }
+    );
+
   });
 });
