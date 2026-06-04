bad6d80b4b4bf3ecc6a19c14aa6fad530fcc8cf5
BlackB34rd <contact@the-tiny-spark.com>
2026-06-04 16:51:03 +0200

    add ngzone theory example

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -4,6 +4,52 @@ import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from '../navigation/nav-bar/nav-bar.component';
 import { PreloadingComponent } from '../navigation/preloading/preloading.component';
 
+
+class ExampleComponent{
+  private _oldState:any = {};
+  private _state:any = {};
+
+  // quand l'état d'un composant est déterminé 
+  // on part du principe qu'il est gelé (on ne peut plus le modifier)
+  // si l'on souhaite changer du composant alors on le clone
+  // puis on écrase les valeurs commune entre les deux états (courants et nouveaux)
+  // par les valeurs du nouvel état, puis on gèle le nouvel état ainsi crée. 
+
+  public setState( newState:any ){
+    let clone:any = {}; 
+    for( var prop in this._state){
+      clone[prop] = this._state[prop];
+    }
+
+    for( var prop in newState){
+      clone[prop] = newState[prop];
+    }
+    // this._state = {...this._state, ...newState}; 
+
+    // on gèle l'état
+    Object.freeze(this._state);
+  }
+
+  public getState(){
+    return this._state;
+  }
+
+  public needsUpdate(){
+      return!( this._oldState === this._state );
+  }
+
+  // il est alors facile de vérifier si le composant a besoin d'être actualisé
+  // si l'ancien état n'est pas le même que l'état en cours , alors on actualise
+  // la vue et on dit que l'ancien état est égal au nouvel état
+  public update(){
+    if( this._oldState === this._state )
+      return false;
+
+    this._oldState = this._state;
+    return true;
+  }
+}
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -14,43 +60,24 @@ import { PreloadingComponent } from '../navigation/preloading/preloading.compone
 })
 export class AppComponent {
   public title:string = 'My Video Game Store';
-  public price:number = 0; 
-
-  private _lastRefreshTime = new Date().getTime();
-  private _ref = inject(ChangeDetectorRef); 
-  private _ngZone = inject(NgZone);
-
 
   public ngOnInit(){
-    // this._ref.detach();
-    // this._ref.detectChanges();
 
-    // this._ngZone.runOutsideAngular(
-    //   ()=>{
-        setInterval( 
-          ()=>{
-              this.price++;
-              // condition pour rafrâichir le template. 
-              // changeDetectorRef sert à définir une nouvelle politique 
-              // de rafraîchissement des composants afin de sauvegarder 
-              // des performances sur les composants qui changent leur donnée
-              // à très haute fréquence
-              if( new Date().getTime() - this._lastRefreshTime >= 1000){
-                this._lastRefreshTime = new Date().getTime();
+    const comp = new ExampleComponent();
+    comp.setState({name: "Merlin"}); 
+    console.log(comp.needsUpdate(), comp.getState());
+    comp.update();
+    console.log(comp.needsUpdate(), comp.getState());
 
-                // this._ngZone.runTask(
-                //   ()=>{
-                    this._ref.markForCheck();
-                    // this._ref.detectChanges();
-                //   }
-                // )
-              }
-          }, 
-          100
-        );
-    //   }
-    // )
-    
+    comp.setState({name: "Arthur", age: "30", weapon: "sword"}); 
+    console.log(comp.needsUpdate(), comp.getState());
+    comp.update();
+    console.log(comp.needsUpdate(), comp.getState());
+
+    comp.setState({name: "Guenièvre", age: "32"}); 
+    console.log(comp.needsUpdate(), comp.getState());
+    comp.update();
+    console.log(comp.needsUpdate(), comp.getState());
 
   }
 }
